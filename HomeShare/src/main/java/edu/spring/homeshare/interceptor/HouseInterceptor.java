4692e96ae8268c39193788f415bedaddd3e03457
jun <jun@DESKTOP-A9KNOAF>
2020-06-15 17:54:07 +0900

    인터셉터 예외처리

--- HomeShare/src/main/java/edu/spring/homeshare/interceptor/HouseInterceptor.java
+++ HomeShare/src/main/java/edu/spring/homeshare/interceptor/HouseInterceptor.java
@@ -18,46 +18,47 @@ import edu.spring.homeshare.service.HouseService;
 //최상위 인터셉터
 public class HouseInterceptor extends HandlerInterceptorAdapter {
 	private static final Logger logger = LoggerFactory.getLogger(HouseInterceptor.class);
-	
+
 	@Autowired
 	private HouseService houseService;
-	
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		boolean masterFlag = false;
 		String reqUrl = request.getRequestURL().toString(); //전송받
-		
-		logger.info("house interceptor 실행");
-	
-		//세션에서 정보가져오기
-		
-		HttpSession session = request.getSession();		
-		MemberVO membervo = (MemberVO) session.getAttribute("memberVO");
-		if(membervo !=null) {
-			logger.info("membervo : " + membervo);
-		}else {
-			
-			logger.info("membervo is null");
-		}
-		
 
-		if(membervo == null) { // member세션이 없음 : 로그아웃상태
-			masterFlag = false;
-			//뷰 메시지 출력하기
-			PrintWriter out = response.getWriter();
-			out.print("<head>" + "<meta charset='UTF-8'>" + "</head>");
-			out.print("<script>alert('권한이 없습니다.');</script>");
-			
-			
-			response.sendRedirect(request.getContextPath() + "/");
-			logger.info("세션이 없습니다. 메인페이지 가기"); //세션이 없습니다.
-			
-			
-		}else {
-			if(reqUrl.contains("house-update")) {
+		logger.info("house interceptor 실행");
+
+		//세션에서 정보가져오기
+
+		HttpSession session = request.getSession();
+		MemberVO membervo = (MemberVO) session.getAttribute("memberVO");
+
+		if (membervo == null) { // member세션이 없음 : 로그아웃상태
+			if (reqUrl.contains("list")) {
+				logger.info("house-list 예외처리");
+				masterFlag = true;
+			} else if (reqUrl.contains("display")) {
+				logger.info("display 예외처리");
+				masterFlag = true;
+			} else if (reqUrl.contains("detail")) {
+				logger.info("detail 예외처리");
+				masterFlag = true;
+			} else {
+				masterFlag = false;
+				//뷰 메시지 출력하기
+				PrintWriter out = response.getWriter();
+				out.print("<head>" + "<meta charset='UTF-8'>" + "</head>");
+				out.print("<script>alert('권한이 없습니다.');</script>");
+
+				response.sendRedirect(request.getContextPath() + "/");
+				logger.info("세션이 없습니다. 메인페이지 가기"); //세션이 없습니다.
+			}
+
+		} else {
+			if (reqUrl.contains("house-update")) {
 				logger.info("house-update 포함");
-			}else {
+			} else {
 				logger.info("house-update 미포함");
 				logger.info("reqUrl : " + reqUrl);
 			}
@@ -66,32 +67,30 @@ public class HouseInterceptor extends HandlerInterceptorAdapter {
 		}
 		return masterFlag;
 	}
-	
-	
-	
-//	@Override
-//	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//		boolean masterFlag = false;
-//		HttpSession session = request.getSession();		
-//		MemberVO membervo = (MemberVO) session.getAttribute("memberVO");
-//		
-//		logger.info("houseinterceptor post 호출");
-//		if(membervo == null) { // member세션이 없음 : 로그아웃상태
-//			masterFlag = false;
-//			response.sendRedirect(request.getContextPath() + "/");
-//			
-//			//뷰 메시지 출력하기
-//			PrintWriter out = response.getWriter();
-//			out.print("<head>" + "<meta charset='UTF-8'>" + "</head>");
-//			out.print("<script>alert('권한이 없습니다.');</script>");
-//			
-//			
-//			
-//			logger.info("post handler 로그아웃 상태로 보내기");
-//		}else {
-//			masterFlag = true;
-//			logger.info("로그인상태 상태로 보내기");
-//		}
-//	}
+
+	//	@Override
+	//	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+	//		boolean masterFlag = false;
+	//		HttpSession session = request.getSession();		
+	//		MemberVO membervo = (MemberVO) session.getAttribute("memberVO");
+	//		
+	//		logger.info("houseinterceptor post 호출");
+	//		if(membervo == null) { // member세션이 없음 : 로그아웃상태
+	//			masterFlag = false;
+	//			response.sendRedirect(request.getContextPath() + "/");
+	//			
+	//			//뷰 메시지 출력하기
+	//			PrintWriter out = response.getWriter();
+	//			out.print("<head>" + "<meta charset='UTF-8'>" + "</head>");
+	//			out.print("<script>alert('권한이 없습니다.');</script>");
+	//			
+	//			
+	//			
+	//			logger.info("post handler 로그아웃 상태로 보내기");
+	//		}else {
+	//			masterFlag = true;
+	//			logger.info("로그인상태 상태로 보내기");
+	//		}
+	//	}
 
 }
